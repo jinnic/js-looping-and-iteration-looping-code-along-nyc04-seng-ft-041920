414ad1c264696665be7cc01b600d82795ff4633c
jinnic <yoohyojin@gmail.com>
2020-06-19 21:00:08 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(name, msg){
+  const card = []
+  for(let i=0; i<name.length; i++){
+    card.push(`Thank you, ${name[i]}, for the wonderful ${msg} gift!`)
+  }
+  return card
+}
+function countDown(num){
+  while (num>=0 ){
+    console.log(num);
+    num --
+  }
+  
+
+}
